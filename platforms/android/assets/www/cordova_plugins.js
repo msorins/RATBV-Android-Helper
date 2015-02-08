cdf8db0f86115e42e21472411f1671a51c2e5411
Mircea Sorin Sebastian <sorynsoo@gmail.com>
2015-02-08 19:21:08 +0200

    apk
    apk

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,17 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.brodysoft.sqlitePlugin/www/SQLitePlugin.js",
+        "id": "com.brodysoft.sqlitePlugin.SQLitePlugin",
+        "clobbers": [
+            "SQLitePlugin"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.brodysoft.sqlitePlugin": "1.0.3"
+}
+// BOTTOM OF METADATA
+});
